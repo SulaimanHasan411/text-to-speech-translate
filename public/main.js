eefdd7a961913d583336ec251c6a594d495e1f1d
Sulaiman <SulaimanHasan411@gmail.com>
2025-03-29 14:23:49 -0400

    allow user to change voice of tts

--- public/main.js
+++ public/main.js
@@ -1,9 +1,26 @@
-const voiceSelector = document.querySelector("#voiceSelect")
-const playButton = document.querySelector("#playButton")
-const textInput = document.querySelector("textArea")
+const voiceSelector = document.querySelector("#voiceSelect");
+const playButton = document.querySelector("#playButton");
+const textInput = document.querySelector("textArea");
+
+// load voices in
+let voices = [];
+function loadVoices(){
+    voices = speechSynthesis.getVoices();
+    voiceSelector.innerHTML = voices
+        .map((voice, index) => `<option value="${index}">${voice.name} (${voice.lang})</option>`)
+        .join("");
+}
+
+//trigger loading voices when they become available 
+speechSynthesis.onvoiceschanged = loadVoices;
+loadVoices();
 
 // tts
 playButton.addEventListener("click", () => {
-    const utterance = new SpeechSynthesisUtterance(textInput.value)
-    speechSynthesis.speak(utterance)
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoice = voices[voiceSelector.value];
+    if (selectedVoice){
+        utterance.voice = selectedVoice;
+    }
+    speechSynthesis.speak(utterance);
 })
