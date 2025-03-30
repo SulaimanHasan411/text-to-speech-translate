dd1789a9cd8e200fc181f0de6228e2164f6e70c6
Sulaiman <SulaimanHasan411@gmail.com>
2025-03-30 00:25:42 -0400

    setup dotenv

--- api/translate.js
+++ api/translate.js
@@ -1,4 +1,12 @@
-//where the translate severless function will go
+//serverless function
+
+if(process.env.NODE_ENV !== "production"){
+    (async () => {
+        const dotenv = await import("dotenv");
+        dotenv.config();
+    })()
+}
+
 export default async function handler(req, res){
     if (req.method !== "POST"){
         return res.status(405).json({error: "Method Not Allowed: has to be a post request"});
