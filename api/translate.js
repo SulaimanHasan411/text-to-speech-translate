d07ec4779cbb17d9c6567a98b5e0dcdc6a76a381
Sulaiman <SulaimanHasan411@gmail.com>
2025-03-30 00:12:24 -0400

    create serverless function

--- api/translate.js
+++ api/translate.js
@@ -1,1 +1,13 @@
 //where the translate severless function will go
+export default async function handler(req, res){
+    if (req.method !== "POST"){
+        return res.status(405).json({error: "Method Not Allowed: has to be a post request"});
+    }
+
+    const {text, targetLang} = req.body;
+
+    if(!text || !targetLang){
+        return res.status(400).json({error: "Missing text or language"});
+    }
+
+}
