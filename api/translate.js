0a42c237521c3719bd6116170b902b18bd7b7dfa
Sulaiman <SulaimanHasan411@gmail.com>
2025-03-30 00:41:49 -0400

    make api request

--- api/translate.js
+++ api/translate.js
@@ -18,4 +18,31 @@ export default async function handler(req, res){
         return res.status(400).json({error: "Missing text or language"});
     }
 
+    const apiKey = process.env.GOOGLE_TRANSLATE_API_KEY
+    const apiUrl = "https://translation.googleapis.com/language/translate/v2?key=${apiKey}"
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: "POST", 
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                q: text,
+                tagetLang,
+                format: "text"
+            })
+        });
+
+        if(!response.ok){
+            const errorText = await response.text();
+            return res.status(response.status).json({error: errorText});
+        }
+
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error in API call: ", error);
+        res.status(500).json({error: "Internal Server Error (Api call)"});
+    }
 }
